35f4e05e54a6d83ad302cdda3a005f598edebb12
Alastair Swan <alastair1008@gmail.com>
2024-10-18 16:59:49 -0700

    Changed UI text and coloring for Motor current monitoring component

--- src/app/ClientComponents/CurrentState.tsx
+++ src/app/ClientComponents/CurrentState.tsx
@@ -4,7 +4,7 @@ import { MotorParams } from "../MotorControlClient";
 export function CurrentState({ state }: { state: MotorParams }){
     return (
         <Box>
-            { state.ISD ? <span style={{color: 'red'}}>Overcurrent</span> : "Normal" }
+            Driver Current: { state.ISD ? <span style={{color: 'red'}}>Overcurrent</span> : <span style={{color: '#00FF00'}}>Normal</span> }
         </Box>
     )
 }
