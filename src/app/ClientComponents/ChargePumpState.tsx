d3069d42c9ecf8f53f21a99e047991951ced9afb
Alastair Swan <alastair1008@gmail.com>
2024-10-18 17:00:23 -0700

    Changed coloring and text formatting for charge pump monitoring component

--- src/app/ClientComponents/ChargePumpState.tsx
+++ src/app/ClientComponents/ChargePumpState.tsx
@@ -6,7 +6,7 @@ import { Box } from "@mui/material"
 
 export function ChargePumpState({ state }: { state: MotorParams }){
     return (
-        <Box>
+        <Box width={'100%'} whiteSpace={'nowrap'}>
             Charge Pump: { state.VCP_MASK ? (state.CP_LOW ? <span style={{color: 'red'}}>Low</span> : "Normal" ) : <span style={{color: 'yellow'}}>Not Monitored</span> }
         </Box>
     )
