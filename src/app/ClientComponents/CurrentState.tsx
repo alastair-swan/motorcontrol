f4061bdf37bf088bdeb92aeccd029ccbb70af046
Alastair Swan <alastair1008@gmail.com>
2024-10-19 22:39:28 -0700

    updated to show when the current is not being monitored

--- src/app/ClientComponents/CurrentState.tsx
+++ src/app/ClientComponents/CurrentState.tsx
@@ -1,11 +1,18 @@
 import { Box } from "@mui/material";
 import { MotorParams } from "../MotorControlClient";
-import { errorStateColor, goodStateColor } from "../UIStyle";
+import { errorStateColor, goodStateColor, warningStateColor } from "../UIStyle";
 
 export function CurrentState({ state }: { state: MotorParams }){
     return (
         <Box>
-            Driver Current: { state.ISD ? <span style={{color: errorStateColor}}>Overcurrent</span> : <span style={{color: goodStateColor}}>Normal</span> }
+            Driver Current: { 
+                state.ISD_MASK ? (
+                    <span style={{color: warningStateColor}}>Not Monitored</span>
+                ) : (
+                    state.ISD ? 
+                        <span style={{color: errorStateColor}}>Overcurrent</span> : 
+                        <span style={{color: goodStateColor}}>Normal</span> 
+                )}
         </Box>
     )
 }
