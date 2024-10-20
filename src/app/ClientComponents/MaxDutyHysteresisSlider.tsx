a1272c134a8e46711eb3c95410ddc93119ffa372
Alastair Swan <alastair1008@gmail.com>
2024-10-19 22:38:57 -0700

    updated text to show calculated hysteresis values, also inverted slider so that the behavior is as expected

--- src/app/ClientComponents/MaxDutyHysteresisSlider.tsx
+++ src/app/ClientComponents/MaxDutyHysteresisSlider.tsx
@@ -4,28 +4,38 @@ import { sliderComponentProps, RegisterList } from "."
 import { UpdateParam } from "../MotorControl"
 import { Box, Slider } from '@mui/material'
 import { componentStyle } from "../UIStyle"
+import { asPercentage } from "./helper"
 
 // MAXDUTYHYS
 export function MaxDutyHysteresisSlider ({ motorNumber, state, setState, frameStyle = componentStyle }: sliderComponentProps){
+    
+    const scaleFunction = (value: number) => { return ((state.MAXDUTY + 257 - (15 - value + 1) * 2) / 512) }
+    const formatText = (value: number) => {
+        return asPercentage(value)
+    }
+    
     const switchText = () => {
-        return state.MAXDUTYHYS
+        return formatText(scaleFunction(15-state.MAXDUTYHYS))
     }
     return (
         <Box sx={ frameStyle }>
             Hysteresis at Max Duty: {switchText()}
             <Slider 
+                track={"inverted"}
                 valueLabelDisplay='auto' 
-                value={ state.MAXDUTYHYS }
+                value={ 15-state.MAXDUTYHYS }
                 min={0}
                 max={15}
                 step={1}
+                scale={scaleFunction}
+                valueLabelFormat={formatText}
                 onChange={(event: Event, newValue: number | number[]) => {
                     if (typeof newValue === 'number'){
                         setState({
                             ...state,
-                            MAXDUTYHYS: newValue
+                            MAXDUTYHYS: 15-newValue
                         })  
-                        UpdateParam(motorNumber, RegisterList.MAXDUTYHYS.command, newValue)
+                        UpdateParam(motorNumber, RegisterList.MAXDUTYHYS.command, 15 - newValue)
                     }
                 }}
             />
