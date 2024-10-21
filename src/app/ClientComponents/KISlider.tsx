d9943981c61e906c11f8492332b6a1ebf3b1eede
Alastair Swan <alastair1008@gmail.com>
2024-10-20 17:04:04 -0700

    Modified sliders for PID control to have the full range and automatically update the 8x bit and switches

--- src/app/ClientComponents/KISlider.tsx
+++ src/app/ClientComponents/KISlider.tsx
@@ -1,36 +1,66 @@
 "use client"
 
-import { sliderComponentProps, RegisterList } from "."
+import { sliderComponentProps } from "."
 import { UpdateParam } from "../MotorControl"
 import { Box, Slider } from '@mui/material'
 import { componentStyle } from "../UIStyle"
+import { KI, KIX } from "./Register"
 
 // KI
 export function KISlider ({ motorNumber, state, setState, frameStyle = componentStyle }: sliderComponentProps){
-    const sliderScale = (value: number) => { return (state.KIX ? value * 0.08 * 8: value * 0.08) }
+    const sliderScale = (value: number) => { 
+        return value * 0.08 
+    }
     const sliderFormat = (value: number) => {
         return value.toFixed(2)
     }
     const switchText = () => {
-        return sliderFormat(sliderScale(state.KI))
+        return sliderFormat(sliderScale(state.KIX ? state.KI * 8 : state.KI))
     }
     return (
         <Box sx={ frameStyle }>
             I coefficient: {switchText()}
             <Slider 
                 valueLabelDisplay='auto' 
-                value={ state.KI }
-                min={0} 
-                max={127}
-                step={1}
+                value={ state.KIX ? state.KI * 8 : state.KI }
+                min={ KI.min } 
+                max={ KI.max as number * 8}
+                step={ state.KI <= (KI.max as number) ? 1 : 8 }
                 scale={sliderScale}
                 onChange={(event: Event, newValue: number | number[]) => {
                     if (typeof newValue === 'number'){
-                        setState({
-                            ...state,
-                            KI: newValue
-                        })
-                        UpdateParam(motorNumber, RegisterList.KI.command, newValue)
+                        if (newValue <= (KI.max as number) && state.KIX){
+                            setState({
+                                ...state,
+                                KI: newValue,
+                                KIX: false
+                            })
+                            UpdateParam(motorNumber, KI, newValue)
+                            UpdateParam(motorNumber, KIX, false)
+                        }
+                        else if (newValue > (KI.max as number) && !state.KIX){
+                            setState({
+                                ...state,
+                                KI: Math.round(newValue / 8),
+                                KIX: true
+                            })
+                            UpdateParam(motorNumber, KI, Math.round(newValue / 8))
+                            UpdateParam(motorNumber, KIX, true)
+                        }
+                        else if (newValue <= (KI.max as number)){
+                            setState({
+                                ...state,
+                                KI: newValue
+                            })
+                            UpdateParam(motorNumber, KI, newValue)
+                        }
+                        else{
+                            setState({
+                                ...state,
+                                KI: Math.round(newValue / 8)
+                            })
+                            UpdateParam(motorNumber, KI, Math.round(newValue / 8))
+                        }
                     }
                 }}
                 valueLabelFormat={sliderFormat}
--- src/app/ClientComponents/KPSlider.tsx
+++ src/app/ClientComponents/KPSlider.tsx
@@ -1,36 +1,66 @@
 "use client"
 
-import { sliderComponentProps, RegisterList } from "."
+import { sliderComponentProps } from "."
 import { UpdateParam } from "../MotorControl"
 import { Box, Slider } from '@mui/material'
 import { componentStyle } from "../UIStyle"
+import { KP, KPX } from "./Register"
 
 // KP
 export function KPSlider ({ motorNumber, state, setState, frameStyle = componentStyle }: sliderComponentProps){
-    const sliderScale = (value: number) => { return (state.KPX ? value * 0.08 * 8 : value * 0.08) }
+    const sliderScale = (value: number) => { 
+        return value * 0.08 
+    }
     const sliderFormat = (value: number) => {
         return value.toFixed(2)
     }
     const switchText = () => {
-        return sliderFormat(sliderScale(state.KP))
+        return sliderFormat(sliderScale(state.KPX ? state.KP * 8 : state.KP))
     }
     return (
         <Box sx={ frameStyle }>
             I coefficient: {switchText()}
             <Slider 
                 valueLabelDisplay='auto' 
-                value={ state.KP }
-                min={0} 
-                max={127}
-                step={1}
+                value={ state.KPX ? state.KP * 8 : state.KP }
+                min={ KP.min } 
+                max={ KP.max as number * 8}
+                step={ state.KP <= (KP.max as number) ? 1 : 8 }
                 scale={sliderScale}
                 onChange={(event: Event, newValue: number | number[]) => {
                     if (typeof newValue === 'number'){
-                        setState({
-                            ...state,
-                            KP: newValue
-                        })  
-                        UpdateParam(motorNumber, RegisterList.KP.command, newValue)
+                        if (newValue <= (KP.max as number) && state.KPX){
+                            setState({
+                                ...state,
+                                KP: newValue,
+                                KPX: false
+                            })
+                            UpdateParam(motorNumber, KP, newValue)
+                            UpdateParam(motorNumber, KPX, false)
+                        }
+                        else if (newValue > (KP.max as number) && !state.KPX){
+                            setState({
+                                ...state,
+                                KP: Math.round(newValue / 8),
+                                KPX: true
+                            })
+                            UpdateParam(motorNumber, KP, Math.round(newValue / 8))
+                            UpdateParam(motorNumber, KPX, true)
+                        }
+                        else if (newValue <= (KP.max as number)){
+                            setState({
+                                ...state,
+                                KP: newValue
+                            })
+                            UpdateParam(motorNumber, KP, newValue)
+                        }
+                        else{
+                            setState({
+                                ...state,
+                                KP: Math.round(newValue / 8)
+                            })
+                            UpdateParam(motorNumber, KP, Math.round(newValue / 8))
+                        }
                     }
                 }}
                 valueLabelFormat={sliderFormat}
