2f03f89f631cce2ebc87755ec143356b98cda615
Alastair Swan <alastair1008@gmail.com>
2024-10-22 17:20:22 -0500

    updated to indicate when the error is inhibited

--- src/app/ClientComponents/StartupState.tsx
+++ src/app/ClientComponents/StartupState.tsx
@@ -2,12 +2,15 @@
 
 import { Box } from "@mui/material";
 import { MotorParams } from "../MotorControlClient";
-import { errorStateColor, goodStateColor } from "../UIStyle";
+import { errorStateColor, goodStateColor, warningStateColor } from "../UIStyle";
 
 export function StartupState({ state }: {state: MotorParams}){
     return (
         <Box>
-            Motor Start: { state.ST_FAIL ? <span style={{color: errorStateColor}}>Failed</span> : <span style={{color: goodStateColor}}>Successful</span>}
+            Motor Start: { 
+                !state.LOCKDIS ? <span style={{color: warningStateColor}}>Not Monitored</span> :
+                state.ST_FAIL ? <span style={{color: errorStateColor}}>Failed</span> : 
+                <span style={{color: goodStateColor}}>Successful</span>}
         </Box>
     )
 }
