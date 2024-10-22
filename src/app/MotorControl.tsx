487250d4ebd549aac4fe647ef28e6bfb9580794f
Alastair Swan <alastair1008@gmail.com>
2024-10-22 13:36:45 -0500

    added simulate option to motor control to get back plausible values for testing even when the motor is not connected

--- src/app/MotorControl.tsx
+++ src/app/MotorControl.tsx
@@ -1,19 +1,43 @@
 "use server"
 
 import { ServerRegister } from "./ClientComponents"
+import { HZ_CNT, MAXSPEED } from "./ClientComponents/Register"
 
 
 //const native = require('/home/alastair/git/motorcontrol/build/Release/native.node')
 const native = __non_webpack_require__('../../../build/Release/native.node')
 
+const simulate = true
+
 export async function UpdateParam(motorNumber: number, paramName: string, paramValue: number | string | boolean): Promise<string> {
-    const result = native.setParam(motorNumber, paramName, paramValue)
-    console.log('update: ' + paramName + ' on Motor: ' + motorNumber + ' to ' + paramValue + ' with result: ' + result)
-    return result
+    if (simulate){
+        console.log('simulated update: ' + paramName + ' on Motor: ' + motorNumber + ' to ' + paramValue)
+        return 'simulated update: ' + paramName + ' on Motor: ' + motorNumber + ' to ' + paramValue
+    }
+    else{
+        const result = native.setParam(motorNumber, paramName, paramValue)
+        console.log('update: ' + paramName + ' on Motor: ' + motorNumber + ' to ' + paramValue + ' with result: ' + result)
+        return result
+    }
+
 }
 
 export async function GetParam(motorNumber: number, paramName: string): Promise<number> {
-    const result = native.getParam(motorNumber, paramName)
-    console.log('readback: ' + paramName + ' from Motor: ' + motorNumber + ' with result: ' + result)
-    return result
+    if (simulate){
+        if (paramName === HZ_CNT.command){
+            const result = Math.random() * 32768
+            console.log('simulated readback: ' + paramName + ' from Motor: ' + motorNumber + " with result: " + result)
+            return result
+        }
+        else {
+            const result = native.getParam(motorNumber, paramName)
+            console.log('readback: ' + paramName + ' from Motor: ' + motorNumber + ' with result: ' + result)
+            return result
+        }
+    }
+    else{
+        const result = native.getParam(motorNumber, paramName)
+        console.log('readback: ' + paramName + ' from Motor: ' + motorNumber + ' with result: ' + result)
+        return result
+    }
 }
