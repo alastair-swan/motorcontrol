e7c43d532dcc1dd333d5c9c5f066b0ffffbd72bc
Alastair Swan <alastair1008@gmail.com>
2024-10-23 00:19:51 -0700

    added ability to manually configure simulated readback

--- src/app/MotorControl.tsx
+++ src/app/MotorControl.tsx
@@ -5,32 +5,32 @@ import { CP_LOW, HZ_CNT, ISD, UD_SPD, OV_SPD, ST_FAIL, TSD } from "./ClientCompo
 //const native = require('/home/alastair/git/motorcontrol/build/Release/native.node')
 const native = __non_webpack_require__('../../../build/Release/native.node')
 
-const simulate = true
-
 export type updateValues = {
+    simulated: boolean,
     HZ_CNT: number,
-    CP_LOW: boolean,
-    TSD: boolean,
-    ISD: boolean,
-    OV_SPD: boolean,
-    UD_SPD: boolean,
-    ST_FAIL: boolean
+    CP_LOW: number,
+    TSD: number,
+    ISD: number,
+    OV_SPD: number,
+    UD_SPD: number,
+    ST_FAIL: number
 }
 
-export async function getData(motorNumber: number): Promise<updateValues>{
+export async function getData(motorNumber: number, simulated = false): Promise<updateValues>{
     return {
-        HZ_CNT: await GetParam(motorNumber, HZ_CNT.command),
-        CP_LOW: (await GetParam(motorNumber, CP_LOW.command)) === 1,
-        TSD: (await GetParam(motorNumber, TSD.command)) === 1,
-        ISD: (await GetParam(motorNumber, ISD.command)) === 1,
-        OV_SPD: (await GetParam(motorNumber, OV_SPD.command)) === 1,
-        UD_SPD: (await GetParam(motorNumber, UD_SPD.command)) === 1,
-        ST_FAIL: (await GetParam(motorNumber, ST_FAIL.command)) === 1
+        simulated: simulated,
+        HZ_CNT: await GetParam(motorNumber, HZ_CNT.command, simulated),
+        CP_LOW: (await GetParam(motorNumber, CP_LOW.command, simulated)),
+        TSD: (await GetParam(motorNumber, TSD.command, simulated)),
+        ISD: (await GetParam(motorNumber, ISD.command, simulated)),
+        OV_SPD: (await GetParam(motorNumber, OV_SPD.command, simulated)),
+        UD_SPD: (await GetParam(motorNumber, UD_SPD.command, simulated)),
+        ST_FAIL: (await GetParam(motorNumber, ST_FAIL.command, simulated))
     } as updateValues
 }
 
-export async function UpdateParam(motorNumber: number, paramName: string, paramValue: number | string | boolean): Promise<string> {
-    if (simulate){
+export async function UpdateParam(motorNumber: number, paramName: string, paramValue: number | string | boolean, simulated = false): Promise<string> {
+    if (simulated){
         console.log('simulated update: ' + paramName + ' on Motor: ' + motorNumber + ' to ' + paramValue)
         return 'simulated update: ' + paramName + ' on Motor: ' + motorNumber + ' to ' + paramValue
     }
@@ -42,12 +42,30 @@ export async function UpdateParam(motorNumber: number, paramName: string, paramV
 
 }
 
-export async function GetParam(motorNumber: number, paramName: string): Promise<number> {
-    if (simulate){
+export async function GetParam(motorNumber: number, paramName: string, simulated = false): Promise<number> {
+    if (simulated){
         if (paramName === HZ_CNT.command){
             const result = 10001-((new Date().getTime()) % 10000)
             console.log('simulated readback: ' + paramName + ' from Motor: ' + motorNumber + " with result: " + result)
             return result
+        }
+        else if (paramName === CP_LOW.command){
+            return 0
+        }
+        else if (paramName === TSD.command){
+            return 0
+        }
+        else if (paramName === ISD.command){
+            return 0
+        }
+        else if (paramName === OV_SPD.command){
+            return 0
+        }
+        else if (paramName === ST_FAIL.command){
+            return 0
+        }
+        else if (paramName === HZ_CNT.command){
+            return 0
         }
         else {
             const result = native.getParam(motorNumber, paramName)
