54092623291cc1b41ea5f7197feeaa0ed358b67d
Alastair Swan <alastair1008@gmail.com>
2024-10-28 07:44:32 -0700

    changed so that there will be multiple colors based on reported value

--- src/app/ClientComponents/RotationState.tsx
+++ src/app/ClientComponents/RotationState.tsx
@@ -6,18 +6,15 @@ import { HZ_CNT } from "./Register";
 export function RotationState({state}: {state: MotorParams}){
     const getRPMText = () => {
         return (
-            Number.isNaN(state.hz_cnt) || HZ_CNT.normalize(state.hz_cnt) / state.POLEPAIR * 60 === 2500000 ? " No Info" : 
-            state.hz_cnt === 65535 ? "0 RPM" :
-            " " + Math.round(HZ_CNT.normalize(state.hz_cnt) / state.POLEPAIR * 60) + " RPM"
+            Number.isNaN(state.hz_cnt) || HZ_CNT.normalize(state.hz_cnt) / state.POLEPAIR * 60 === 2500000 ? <span style={{color: warningStateColor}}>No Info</span> : 
+            state.hz_cnt === 65535 ? <span style={{color: warningStateColor}}>0 RPM</span> :
+            <span>{Math.round(HZ_CNT.normalize(state.hz_cnt) / state.POLEPAIR * 60) } RPM</span>
         )
     }
     return (
         <Box>
             {state.simulated ? <span style={{color: warningStateColor}}>Simulated </span> : ""}
-            Reported Speed:
-            {
-                <span style={{color: warningStateColor}}>{getRPMText()}</span>
-            }
+            Reported Speed: {getRPMText()}
         </Box>
     )
 }
