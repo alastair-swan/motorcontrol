dcfdeec6d144f8eee48c23b014ef8c4c13c52db0
Alastair Swan <alastair1008@gmail.com>
2024-10-28 01:18:29 -0700

    detect that the motor is not spinning

--- src/app/ClientComponents/RotationState.tsx
+++ src/app/ClientComponents/RotationState.tsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/material";
 import { MotorParams } from "../MotorControlClient";
-import { errorStateColor, warningStateColor } from "../UIStyle";
-import { HZ_CNT, MAXSPEED } from "./Register";
+import { warningStateColor } from "../UIStyle";
+import { HZ_CNT } from "./Register";
 
 export function RotationState({state}: {state: MotorParams}){
     const getRPMText = () => {
         return (
             Number.isNaN(state.hz_cnt) || HZ_CNT.normalize(state.hz_cnt) / state.POLEPAIR * 60 === 2500000 ? " No Info" : 
+            state.hz_cnt === 65535 ? "0 RPM" :
             " " + Math.round(HZ_CNT.normalize(state.hz_cnt) / state.POLEPAIR * 60) + " RPM"
         )
     }
